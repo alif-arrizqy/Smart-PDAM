3d2207afaeab658b825b69eb184a12fed6759436
alif-arrizqy <alif.arrizqy20@gmail.com>
2021-07-21 20:08:54 +0700

    add code description

--- app/Views/user/find_kode.php
+++ app/Views/user/find_kode.php
@@ -6,14 +6,14 @@ foreach ($get_kode->getResult() as $rs) {
     $relay_status = 1;
     $ry['relay_status'] = $relay_status;
 
+    // jika kode ditemukan maka akan menampilkan ini
     echo "findkode = ID Token :", $id_token, " ", "ID User :", $id_user, " ", "Relay :", $relay_status;
+    // lalu meng-update relay_status pada tabel relay menjadi 1 
     $dbs = \Config\Database::connect();
     $st = $dbs->table('relay')->where('id_token', $id_token)->update($ry);
+    // redirect ke halaman relay_aktif
+    // dan juga mengirimkan hasilnya ke nodemcu
     $url = base_url('/User/relay_aktif/' . $id_token . '/' . $id_user . '/' . $relay_status);
     header("refresh:5; url=$url");
 }
 ?>
-
-<!-- <input name="id_user" value="<?= $id_user ?>" class="form-control" hidden>
-<input name="id_token" value="<?= $id_token ?>" class="form-control" hidden>
-<input name="relay_status" value="<?= $relay_status ?>" class="form-control" hidden> -->
